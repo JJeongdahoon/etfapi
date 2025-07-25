a94150339bf788d9b0eaea5b4aa08d17d4c2f09e
JJeongdahoon <147802360+JJeongdahoon@users.noreply.github.com>
2025-07-25 13:28:42 +0900

    Update config.py

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ class Config:
     ROUTER_API = 'https://clovastudio.stream.ntruss.com/v1/routers/7827dx6w/versions/3/route'
 
     # 스킬셋 호출 경로
-    SKILLSET_API_ETF = 'https://clovastudio.stream.ntruss.com/v1/skillsets/iqso4xhn/versions/6/final-answer'
-    SKILLSET_API_NEWS = 'https://clovastudio.stream.ntruss.com/v1/skillsets/pxzveegt/versions/1/final-answer'
+    SKILLSET_API_ETF = 'https://clovastudio.stream.ntruss.com/v1/skillsets/iqso4xhn/versions/3/final-answer'
+    # SKILLSET_API_NEWS = 'https://clovastudio.stream.ntruss.com/v1/skillsets/pxzveegt/versions/1/final-answer'
 
     # ETF 정의된 스킬(API)의 인증 정보
     NAVER_LOCAL_CLIENT_ID = 'xQLkmvq0rQoSavcYk4I8'
@@ -25,7 +25,7 @@ class Config:
 
     REQUEST_ID_ROUTER = '60128f2ecd2f4a4aa2c64e55042d986a' 
 
-    REQUEST_ID_ETF_SKILLSET = '0df09abbc17f404ab85b2976726bf9d9'
+    REQUEST_ID_ETF_SKILLSET = '621f372fd3f041b6b5dd45df2ed0b313'
     REQUEST_ID_NEWS_SKILLSET = '69d15779d1244dcc9da82c1e1b0fb36a' 
 
     REQUEST_ID_CHAT = '4aead600757a48fabe453b9bd4d2f623'
