052264495fcb0a7bbde5f2dccfd7bf55480b5266
JJeongdahoon <147802360+JJeongdahoon@users.noreply.github.com>
2025-07-25 13:12:59 +0900

    Update config.py

--- config.py
+++ config.py
@@ -2,10 +2,10 @@
 
 class Config:
     # 라우터 호출 경로
-    ROUTER_API = 'https://clovastudio.stream.ntruss.com/v1/routers/7827dx6w/versions/2/route'
+    ROUTER_API = 'https://clovastudio.stream.ntruss.com/v1/routers/7827dx6w/versions/3/route'
 
     # 스킬셋 호출 경로
-    SKILLSET_API_ETF = 'https://clovastudio.stream.ntruss.com/v1/skillsets/iqso4xhn/versions/4/final-answer'
+    SKILLSET_API_ETF = 'https://clovastudio.stream.ntruss.com/v1/skillsets/iqso4xhn/versions/6/final-answer'
     SKILLSET_API_NEWS = 'https://clovastudio.stream.ntruss.com/v1/skillsets/pxzveegt/versions/1/final-answer'
 
     # ETF 정의된 스킬(API)의 인증 정보
@@ -14,8 +14,8 @@ class Config:
     
 
     # NEWS
-    NAVER_NEWS_CLIENT_ID = 'pNTHXmoi74W4OTcNsaO8'
-    NAVER_NEWS_CLIENT_SECRET = 'OALH2ZlvRf'
+    # NAVER_NEWS_CLIENT_ID = 'pNTHXmoi74W4OTcNsaO8'
+    # NAVER_NEWS_CLIENT_SECRET = 'OALH2ZlvRf'
 
     # Chat Completions 호출 경로
     CHAT_COMPLETIONS_API = 'https://clovastudio.stream.ntruss.com/v3/chat-completions/HCX-005' 
@@ -23,9 +23,9 @@ class Config:
     # CLOVA Studio API 인증 정보
     API_KEY = 'nv-1199f7fea2a44b0d9fd82fb3f7aa1d0dPrdp'
 
-    REQUEST_ID_ROUTER = 'edd3342bd49c45068ec17ae7ea85efdd' 
+    REQUEST_ID_ROUTER = '60128f2ecd2f4a4aa2c64e55042d986a' 
 
-    REQUEST_ID_ETF_SKILLSET = '0fbfd9ca27e04d78b65b342586834b35'
+    REQUEST_ID_ETF_SKILLSET = '0df09abbc17f404ab85b2976726bf9d9'
     REQUEST_ID_NEWS_SKILLSET = '69d15779d1244dcc9da82c1e1b0fb36a' 
 
     REQUEST_ID_CHAT = '4aead600757a48fabe453b9bd4d2f623'
